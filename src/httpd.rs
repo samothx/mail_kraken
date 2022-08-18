3772ad84ae9fe9eb8fdf1ddff5b2d9adb8078dde
Thomas Runte <thomas@etnur.net>
2022-08-18 16:59:49 +1200

    axum dead end

--- src/httpd.rs
+++ src/httpd.rs
@@ -27,12 +27,36 @@ use mysql_async::{prelude::*, Pool};
 
 // use axum_extra::extract::cookie::PrivateCookieJar;
 
-use axum::response::Html;
+use axum::response::{Html, IntoResponse, Response};
 use log::{debug, error};
 use nix::errno::errno;
 use std::collections::btree_map::Values;
 
 type WebResult<T> = Result<T, axum::http::StatusCode>;
+
+#[derive(Template)]
+#[template(path = "error.html")]
+struct ErrorTemplate {
+    status: &'static str,
+    message: String,
+}
+
+impl ErrorTemplate {
+    pub fn to_response(
+        status: StatusCode,
+        message: String,
+    ) -> axum::http::Result<Response<Html<String>>> {
+        let template = ErrorTemplate {
+            status: status.as_str(),
+            message,
+        };
+
+        match template.render() {
+            Ok(res) => Response::builder().status(status).body(Html::from(res)),
+            Err(e) => Err(anyhow!("failed to render template")),
+        }
+    }
+}
 
 struct SharedData {
     config: Option<Config>,
@@ -43,33 +67,72 @@ struct SharedData {
 #[template(path = "login.html")]
 struct LoginTemplate {}
 
-async fn login_form() -> WebResult<Html<String>> {
-    Ok(Html::from(
-        LoginTemplate {}
-            .render()
-            .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
+async fn login_form() -> axum::http::Result<Response<Html<String>>> {
+    let template = LoginTemplate {};
+    match template.render() {
+        Ok(res) => Response::builder()
+            .status(StatusCode::OK)
+            .body(Html::from(res)),
+        Err(e) => ErrorTemplate::to_response(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+    }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Payload {
     #[serde(rename = "login-name")]
     name: String,
     passwd: String,
 }
 
+#[derive(Template)]
+#[template(path = "admin_dashboard.html")]
+struct AdminDashboard {}
+
 async fn login_handler(
     Form(payload): Form<Payload>,
     mut session: WritableSession,
-) -> WebResult<String> {
+    Extension(state): Extension<Arc<SharedData>>,
+) -> axum::http::Result<Response> {
     if let Some(user) = session.get::<String>("user") {
         session.destroy();
     }
 
+    debug!("payload: {:?}", payload);
     if payload.name.eq("admin") {
-        todo!()
+        if let Some(config) = state.config.as_ref() {
+            if payload.passwd.eq(config.admin_passwd.as_str()) {
+                session
+                    .insert("user", "admin")
+                    .expect("failed to insert user into session");
+                let template = AdminDashboard {};
+                match template.render() {
+                    Ok(res) => Response::builder()
+                        .status(StatusCode::OK)
+                        .body(Html::from(res)),
+
+                    Err(e) => ErrorTemplate::to_response(
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        "failed to create template for AdminDashboard".to_owned(),
+                    ),
+                }
+            } else {
+                ErrorTemplate::to_response(
+                    StatusCode::UNAUTHORIZED,
+                    "please supply a valid password for admin".to_owned(),
+                )
+            }
+        } else {
+            ErrorTemplate::t_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "no config received - please supply a password as parameter".to_owned(),
+            )
+        }
+    } else {
+        ErrorTemplate::to_response(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "not implemented: please login as admin with password".to_owned(),
+        )
     }
-    todo!()
 }
 
 pub async fn serve(args: ServeCmd, mut config: Option<Config>) -> Result<()> {
