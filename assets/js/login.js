cce73c0afd78425b0b22e2a074ae9a0e2d14c290
Thomas Runte <thomas@etnur.net>
2022-08-21 17:38:46 +1200

    wip

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,37 +5,39 @@ $(document).ready(function () {
 	});
 });
 
-async function login() {
-	console.log("login() entered" );
-	const data = {
-		login: $('#login-name').val(),
-		passwd: $('#passwd').val()
-	};
+function login() {
+	return Promise::new (function (resolve) {
+			console.log("login() entered" );
+			const data = {
+				login: $('#login-name').val(),
+				passwd: $('#passwd').val()
+			};
 
-	console.log(`posting login request with data: ${data}` );
+			console.log(`posting login request with data: ${data}` );
 
-	const request = {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data)
+			const request = {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data)
+			}
+
+			fetch('/api/v1/login', request).then(function (response) {
+				console.log(`request successful: ${response.status}`);
+
+				if (data.login === 'admin') {
+					window.location.href = '/admin_dash';
+				} else {
+					window.location.href = '/dash';
+				}
+				resolve()
+			}).catch(function (error) {
+				console.log(error);
+				resolve()
+			});
 	}
-
-	fetch('/api/v1/login', request).then(function (response) {
-		console.log("request successful");
-
-		if (data.login === 'admin') {
-			window.location.href = '/admin_dash';
-		} else {
-			window.location.href = '/dash';
-		}
-		response.text().then(function (text) { console.log(`response body read: ${text}`) })
-
-	}).catch(function (error) {
-		console.log(error);
-	});
-
-	console.log("fetch done");
+)
 }
+
