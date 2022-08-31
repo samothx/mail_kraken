79db8b84bcb8bd6a18cd174eb0cf5f433d5ed75e
Thomas Runte <thomas@etnur.net>
2022-08-31 20:03:07 +1200

    wip

--- src/doveadm/fetch/parser/hdr_parser.rs
+++ src/doveadm/fetch/parser/hdr_parser.rs
@@ -42,17 +42,15 @@ impl Parser for HdrParser {
     ) -> Result<Option<FetchFieldRes>> {
         trace!("parse_first_field: called");
         if let Some(line_buf) = reader.next_line_raw().await? {
-            let line = rfc2047_decoder::decode(line_buf)?;
+            let line = rfc2047_decoder::decode(line_buf)
+                .with_context(|| format!("rfc2047_decoder::decode failed on [{:?}]", line_buf))?;
             trace!("parse_first_field: got line: [{:?}]", line);
             if self.first_line_re.is_match(line.as_str()) {
                 let mut res: Vec<(String, String)> = Vec::new();
                 while let Some(line_buf) = reader.next_line_raw().await? {
                     trace!("parse_first_field: got next line: [{:?}]", line);
                     let line = rfc2047_decoder::decode(line_buf).with_context(|| {
-                        format!(
-                            "failed to decode line: [{}]",
-                            &*String::from_utf8_lossy(&line_buf[..])
-                        )
+                        format!("rfc2047_decoder::decode failed on [{:?}]", line_buf)
                     })?;
                     if let Some(captures) = self.subseq_line_re.captures(line.as_str()) {
                         trace!("parse_first_field: adding tagged string");
